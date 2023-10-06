545421899b64e887613a35e1d1d27bf2dceb3d1d
Michel <106674127+MichelZero@users.noreply.github.com>
2023-10-06 19:33:15 -0300

    up

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import time
 import threading
 from pynput.mouse import Button, Controller
 from pynput.keyboard import Listener, KeyCode
+from playsound import playsound as playsom
 
-TOGGLE_KEY = KeyCode(char="t")
-clicking = False
-mouse = Controller()
+tecla = KeyCode(char="t") # tecla que vai ser usada para ativar o clicker
+clicking = False        # variavel que vai ser usada para saber se o clicker esta ativo ou nao
+mouse = Controller()   # variavel que vai ser usada para controlar o mouse
+som = "som.mp3"        # variavel que vai ser usada para armazenar o caminho do som
 
-def clicker():
-    while True:
-        if clicking:
-            mouse.click(Button.left, 1)
-        time.sleep(0.2)
+def clicker(): # funcao que vai ser usada para fazer o clicker
+    playsom = playsom(som) # tocando o som
+    while True: # loop infinito
+        if clicking: # se o clicker estiver ativo
+            mouse.click(Button.left, 1) # clique com o botão esquerdo do mouse
+        time.sleep(0.2) # espere 0.2 segundos
 
-def toggle_event(key):
-    if key == TOGGLE_KEY:
-        global clicking
-        clicking = not clicking
+def toggle_event(key): # função que vai ser usada para ativar e desativar o clicker
+    if key == tecla:    # se a tecla apertada for a tecla que vai ser usada para ativar o clicker
+        global clicking  # variável que vai ser usada para saber se o clicker esta ativo ou nao
+        clicking = not clicking # inverte o valor da variável
 
-click_thread = threading.Thread(target=clicker)
-click_thread.start()
+click_thread = threading.Thread(target=clicker) # criando a thread que vai ser usada para fazer o clicker
+click_thread.start() # iniciando a thread
 
-with Listener(on_press=toggle_event) as listener:
-    listener.join()
+with Listener(on_press=toggle_event) as listener: # criando o listener que vai ser usado para ativar e desativar o clicker
+    listener.join() 
+    
+    
---
+++ tocar_som.py
@@ -0,0 +1,9 @@
+
+# tocando o som ao aperta uma tecla
+# Path: main.py
+
+# importando o arquivo
+from tocar_som import *
+
+  
+# criando a t
