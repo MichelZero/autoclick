d0a7ed4da89d65151292e3d0ccd9d0e77e67f974
Michel <106674127+MichelZero@users.noreply.github.com>
2023-10-11 05:53:06 -0300

    up

---
+++ teste.py
@@ -0,0 +1,24 @@
+
+
+class NumerosPares:
+    def __init__(self, inicio, fim):
+        self.inicio = inicio
+        self.fim = fim
+
+    def obter_pares(self):
+        if self.inicio % 2 != 0:
+            self.inicio += 1  # Garante que o início seja um número par
+
+        pares = {x for x in range(self.inicio, self.fim + 1, 2)}
+        return pares
+
+
+# Exemplo de uso
+inicio = 10
+fim = 20
+
+numeros_pares = NumerosPares(inicio, fim)
+pares = numeros_pares.obter_pares()
+
+print(f'Números pares entre {inicio} e {fim}: {pares}')
+print(type(pares))
